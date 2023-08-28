0f6c7cde7944d80698de095f2d213f5dc0b98702
edwinhuish <edwinhuish@gmail.com>
2023-08-28 11:59:09 +0800

    更新 entry.sh

--- entry.sh
+++ entry.sh
@@ -6,7 +6,7 @@ set -e
 : ${PUID:=1000}
 : ${PGID:=1000}
 
-if ! egrep "^$USERNAME:" /etc/passwd > /dev/null 2>&1; then
+if ! egrep "^$USERNAME:" /etc/passwd >/dev/null 2>&1; then
   echo "不存在用户，开始新增：$USERNAME"
   useradd -s /bin/bash --uid $PUID -m $USERNAME
 fi
@@ -18,7 +18,7 @@ if [ "${PGID}" != "automatic" ] && [ "$PGID" != "$OLD_GID" ]; then
 
   echo "修改 GID: $OLD_GID => $PGID"
 
-  if getent group $PGID > /dev/null 2>&1; then
+  if getent group $PGID >/dev/null 2>&1; then
     echo "已存在 GID， 修改主GID"
     usermod -g $PGID $USERNAME
   else
@@ -38,11 +38,13 @@ fi
 
 if [ -d /entry.d ]; then
   for script in $(ls /entry.d/*.sh | sort); do
-    echo "\n\n========================== Processing $script ==========================\n\n";
-    chmod +x $script;
-    $script || exit 1;
+    echo "\n\n========================== Processing $script ==========================\n\n"
+    /bin/bash $script || exit 1
   done
 fi
 
 # 链式调用下一个 shell
-exec "$@"
+/bin/bash "$@"
+
+# 使用 exec 会导致后面的 script 作为主进程，导致 entry.d 的script都在它之下
+# exec "$@"
